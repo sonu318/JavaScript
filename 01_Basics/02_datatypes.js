b8f27ac077b18317fd22cea645e9e0c3ff377478
sonu318 <91945935+sonu318@users.noreply.github.com>
2024-04-20 14:46:45 +0000

    02_datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";    //treat all JS code as newer version 
+
+//alert(1+1)      // we r using nodejs , not a browser
+
+console.log(1+1)
+
+let name = "riya"
+let rollno = 4
+let isPresent = true 
+
+//number  =>  2 pwr 53
+//bigint
+//string = ""
+//boolean = true/ flase
+//null - it is actually a standalone value 
+//undefined => not defined value to varible 
+//symbol => unique
+
+
+//objet 
+console.log(typeof null)          //op - object
+console.log(typeof undefined)     //op - undefined
