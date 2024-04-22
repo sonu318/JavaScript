607bc5406a6407d2b6813001805f9a88d00aba3a
sonu318 <91945935+sonu318@users.noreply.github.com>
2024-04-22 04:41:08 +0000

    03_conversionOperation

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = true
+
+// console.log(typeof score)
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+
+//"90"   = 90  type - numbetr
+//90     = 90  type - number
+//"90abc"= NaN (not a number)  type - number
+//true   = 1   type - number 
+
+
+let isPresent = "riya"
+
+let booleanIsPresentIn  = Boolean(isPresent)
+console.log(booleanIsPresentIn)
+console.log(typeof booleanIsPresentIn)
+
+
+// ""  - false  type = boolean
+//"riya" - true  type = boolean
