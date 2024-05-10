14043ad73b67d0a51d0f6dd0cb7bb0ba7ff402bc
sonu318 <91945935+sonu318@users.noreply.github.com>
2024-05-10 03:41:48 +0000

    num and math

---
+++ 01_Basics/06_num_and_math.js
@@ -0,0 +1,30 @@
+const score =400
+// console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))                 //decimal value upto 2 digit
+
+
+const otherNum = new Number(123.89)
+// console.log(otherNum.toPrecision(3))            //to round off values 
+
+//==========================Math=============
+
+// console.log(Math)
+// console.log(Math.floor(4.67))
+// console.log(Math.ceil(4.57))
+// console.log(Math.round(4.57))
+
+
+console.log(Math.random())                      //random value between 0 to 1
+console.log((Math.random()*10) + 1)
+console.log(Math.floor((Math.random()*10) + 1))         //round of 
+
+const min = 10 
+const max = 20 
+
+console.log(Math.floor(Math.random() * (max- min +1 )) + min)           //to print random value between any range
+
