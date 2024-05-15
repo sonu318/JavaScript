9e6bcb87776af7455df440b842d9050c41e25b0d
sonu318 <91945935+sonu318@users.noreply.github.com>
2024-05-15 17:43:46 +0000

    functions

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -5,6 +5,8 @@ function sayMyName(){
 
 // sayMyName()             //sayMyName - reference and sayMyName() -- execution
 
+//===================================
+
 function addTwoNum(num1 , num2){
     console.log(num1 + num2)
 }
@@ -32,5 +34,33 @@ function isLoggedIn(username){
     }
     return `${username} just logged in`
 }
+// console.log(isLoggedIn("sonal"))
 
-console.log(isLoggedIn("sonal"))
+
+//==============================rest operator ...
+
+function calculateCartPrice(...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(100,200,300))            //[ 100, 200, 300 ]
+
+const user = {
+    username : "sonal",
+    age : 23
+}
+
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username}  and age is ${anyobject.age}`)
+}
+
+// handleObject(user)
+
+
+const myNewArr = [1,2,4,5]
+
+function returnValue(getArray){
+        return getArray[2]
+}
+
+console.log(returnValue(myNewArr))
