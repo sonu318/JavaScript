07f981545e12e0ae71bbfc57d7d7879b39fafd79
sonu318 <91945935+sonu318@users.noreply.github.com>
2024-05-17 11:59:36 +0000

    scopes

--- 03_Basics/02_Scopes.js
+++ 03_Basics/02_Scopes.js
@@ -4,8 +4,8 @@ const b = 20
 var c = 30
 }
 
-// console.log(a)               //not defined - block scope
-// console.log(b)               //not defined - block scope
+console.log(a)               //not defined - block scope
+console.log(b)               //not defined - block scope
 console.log(c)                  //30--- global scope 
 
 
@@ -18,3 +18,40 @@ if(true){
                                             //inner value : 2
                                             //outer value  100
 console.log("outer value " , a)
+
+// =====closure
+
+function one(){
+    const username = "Sonal"
+
+    function two(){
+        const website = "youtube"           //website is not defined
+        console.log(username)
+    }
+    // console.log(website)
+    // two()
+}
+one()
+
+
+if(true){
+    const username = "sonal"
+    if(username === "sonal"){
+        const website = "youtube"
+        console.log(username + website)         //sonalyoutube
+    }
+    console.log(website)                        // website is not defined
+}
+console.log(username)                               // username is not defined
+
+
+// --------------------- interesting --------------------------
+addone(5)
+function addone(num){                   //can declare function at begin
+    return num+1
+}
+
+addTwo(5)
+const addTwo = function(num){           //when holding function return in variable then cant declare function at begin 
+    return num+2
+}
