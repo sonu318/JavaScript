2cde79d32e9002aefc2efbcbbe184c1865ada38b
Sonal Mane <91945935+sonu318@users.noreply.github.com>
2024-05-30 17:06:23 +0000

    do while loop

--- 05_iterations/two.js
+++ 05_iterations/two.js
@@ -14,3 +14,10 @@ while(arr < myArray.length){
     console.log(`Array ${myArray[arr]}`)
     arr = arr +1
 }
+
+let score = 0
+
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score <=10 )
