a2e27d1eb2cc2b49e37aaa1cdaaee32d5efd5113
Sonal Mane <91945935+sonu318@users.noreply.github.com>
2024-05-30 17:03:39 +0000

    while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,16 @@
+
+let index = 0
+
+while(index <= 10){
+    console.log(`value of index is ${index}`)
+    index = index + 3
+}
+
+
+let myArray = ["Flash" , "Batman" , "Ironmane"]
+
+let arr = 0 
+while(arr < myArray.length){
+    console.log(`Array ${myArray[arr]}`)
+    arr = arr +1
+}
