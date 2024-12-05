be496ef342b53a422018c06e0dd1ff0671ae8b4a
Sonal Mane <91945935+sonu318@users.noreply.github.com>
2024-12-05 17:44:27 +0000

    Classes

--- 10_classes_and_oops/myClasses.js
+++ 10_classes_and_oops/myClasses.js
@@ -18,3 +18,16 @@ const chai = new User("sonal","123","1")
 
 console.log(chai.encryptPassword())
 console.log(chai.changeUsername())
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+const tea = new User("tea","abc","2")
+
+console.log(tea.encryptPassword())
+console.log(tea.changeUsername())
