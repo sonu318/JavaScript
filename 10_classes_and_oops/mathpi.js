cdcb1fe79e31e66985f07909bb973b0c15607e4c
Sonal Mane <91945935+sonu318@users.noreply.github.com>
2024-12-06 17:41:34 +0000

    Object.getOwnPropertyDescriptor

---
+++ 10_classes_and_oops/mathpi.js
@@ -0,0 +1,31 @@
+const descriptor = Object.getOwnPropertyDescriptor(Math,'PI')
+
+console.log(descriptor)
+
+const chai = {
+    name  : "ginger chai",
+    price : 250,
+    isAvailable : true
+}
+
+console.log(Object.getOwnPropertyDescriptor(chai , "name"))          //output {
+                                                                            //     value: 'ginger chai',
+                                                                            //     writable: true,
+                                                                            //     enumerable: true,
+                                                                            //     configurable: true
+                                                                            // }
+
+
+// Object.defineProperty(chai,"name",{
+//     writable : false,
+//     enumerable : false
+// })
+
+// console.log(Object.getOwnPropertyDescriptor(chai , "name")) 
+
+
+for(let [key , value] of Object.entries(chai) ){
+    if(typeof key !== 'function'){
+        console.log(`${key} , ${value}`)
+    }
+}
